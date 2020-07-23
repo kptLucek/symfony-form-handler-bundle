f840bad32e8cb6dec2b47cd156cc5607b18795ca
Marcin <marvcin@gmail.com>
2020-07-23 17:31:30 +0200

    ^5.0 as base symfony version

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -17,8 +17,8 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode    = $treeBuilder->root('form_handler');
+        $treeBuilder = new TreeBuilder('form_handler');
+        $rootNode    = $treeBuilder->getRootNode();
 
         return $treeBuilder;
     }
